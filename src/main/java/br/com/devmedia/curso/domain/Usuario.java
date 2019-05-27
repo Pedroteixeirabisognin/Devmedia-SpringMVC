c9f9a12f7bc5cadcd9fdafec14b1e014cd102268
Pedro Teixeira Bisognin <pedro.mindtek@ibge.gov.br>
2019-05-27 13:34:28 -0300

    Configuração JPA no Spring

--- src/main/java/br/com/devmedia/curso/domain/Usuario.java
+++ src/main/java/br/com/devmedia/curso/domain/Usuario.java
@@ -1,7 +1,16 @@
 package br.com.devmedia.curso.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,35 +18,46 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-public class Usuario {
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank
 	@Size(min = 3, max = 50)
+	@Column(nullable = false, length = 50)
 	private String nome;
-	
+
 	@NotBlank
-	@Size(min = 3, max = 50, message = "Campo requerido entre {min} e {max} caracteres.")	
+	@Size(min = 3, max = 50, message = "Campo requerido entre {min} e {max} caracteres.")
+	@Column(nullable = false, length = 50)
 	private String sobrenome;
-	
+
 	@NotNull(message = "O campo 'data de nascimento' é requerido.")
 	@DateTimeFormat(iso = ISO.DATE)
+	@Column(name = "data_nascimento",nullable = false)
 	private LocalDate dtNascimento;
-	
+
+	@Column(name = "tipo_sexo",nullable = false)
+	@Enumerated(EnumType.STRING)
 	private TipoSexo sexo;
-	
+
 	public Usuario() {
 		super();
-	}	
+	}
 
 	public Usuario(Long id, String nome, String sobrenome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.sobrenome = sobrenome;
-	}	
-	
+	}
+
 	public Usuario(Long id, String nome, String sobrenome, LocalDate dtNascimento) {
 		super();
 		this.id = id;
@@ -45,7 +65,7 @@ public class Usuario {
 		this.sobrenome = sobrenome;
 		this.dtNascimento = dtNascimento;
 	}
-	
+
 	public Usuario(Long id, String nome, String sobrenome, LocalDate dtNascimento, TipoSexo sexo) {
 		super();
 		this.id = id;
@@ -78,7 +98,7 @@ public class Usuario {
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-	
+
 	public LocalDate getDtNascimento() {
 		return dtNascimento;
 	}
@@ -96,9 +116,33 @@ public class Usuario {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Usuario [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + "]";
 	}
-	
-	
+
 }
